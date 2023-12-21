582267e68bee1a4d528b0c9c61d02612e7717b5b
Ryuk420 <62930896+Ryuk420@users.noreply.github.com>
2023-12-20 22:20:59 -0500

    Update auth0Config.js
    Updated deployed server location

--- server/config/auth0Config.js
+++ server/config/auth0Config.js
@@ -1,7 +1,7 @@
 import {auth} from 'express-oauth2-jwt-bearer'
 
 const jwtCheck = auth({
-    audience: "http://localhost:8000",
+    audience: "https://room-finder-project-server.vercel.app",
     issuerBaseURL: "https://dev-4pv0mifib4in2xx4.us.auth0.com",
     tokenSigningAlg: "RS256"
 })
